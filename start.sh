29da5e1eac24a0434098c432eaf518e1492cd46f
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-09 15:18:40 +0300

    removed an extra identation on start.sh file

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-export MOVIE_API_KEY = "de532a169b57a991a617596c281a4b8d"
-export SECRET_KEY = "Mathematics109"
+export MOVIE_API_KEY="de532a169b57a991a617596c281a4b8d"
+export SECRET_KEY="Mathematics109"
+
 python3.8 manage.py server
 
